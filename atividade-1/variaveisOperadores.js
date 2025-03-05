3c05bc2fc0277af020949c3e978262f2877f7da4
Takesh0s <luizphillipedf83@gmail.com>
2025-03-04 21:58:25 -0300

    Primeiro commit

---
+++ atividade-1/variaveisOperadores.js
@@ -0,0 +1,24 @@
+let num1 = 10;
+let num2 = 5;
+let resultadoAdicao = num1 + num2;
+console.log(`Adição entre ${num1} e ${num2}, resultado é ${resultadoAdicao}`);
+
+let num3 = 20;
+let num4 = 8;
+let resultadoSubtracao = num3 - num4;
+console.log(`Subtração entre ${num3} e ${num4}, o resultado é  ${resultadoSubtracao}`);
+
+let num5 = 7;
+let num6 = 3;
+let resultadoMultiplicacao = num5 * num6;
+console.log(`Multiplicação entre ${num5} e ${num6}, o resultado é ${resultadoMultiplicacao}`);
+
+let num7 = 16;
+let num8 = 4;
+let resultadoDivisao = num7 / num8;
+console.log(`Divisão entre ${num7} e ${num8}, o resultado é ${resultadoDivisao}`);
+
+let num9 = 15;
+let num10 = 4;
+let resultadoModulo = num9 % num10;
+console.log(`${num9} e ${num10}, o módulo é ${resultadoModulo}`);
