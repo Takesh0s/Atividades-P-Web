32869b5cb0e35aa0450941bf39291be4afb8d1c7
Takesh0s <luizphillipedf85@gmail.com>
2025-05-20 21:40:43 -0300

    atualização

--- atividade5/index.js
+++ atividade5/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const router = require('./routers/agendamentoRouter');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
